1124427fd21ca48528175332fb0a103a701adfc7
1363025975@qq.com <1363025975@qq.com>
2019-06-30 20:29:01 +0800

    Initial commit

---
+++ src/main/java/com/ledii/pdftools/PdfToolsApplication.java
@@ -0,0 +1,13 @@
+package com.ledii.pdftools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ledii/pdftools/PdfToolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ledii.pdftools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdfToolsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
