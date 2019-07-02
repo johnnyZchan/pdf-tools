67542b9454e062ffaba0d300e79abd2b5d95ab2e
JohnnyChan <1363025975@qq.com>
2019-07-02 21:53:08 +0800

    dataTables commit

--- src/main/java/com/ledi/pdftools/controllers/PdfListController.java
+++ src/main/java/com/ledi/pdftools/controllers/PdfListController.java
@@ -1,8 +1,14 @@
 package com.ledi.pdftools.controllers;
 
+import com.ledi.pdftools.beans.ResponseModel;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Api(tags = "PDF列表接口")
 @RestController
@@ -10,4 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PdfListController extends BaseController {
 
 
+    @GetMapping("/pdf/list")
+    public ResponseModel getPdfList(HttpServletRequest request,
+                                    @RequestHeader(name = "lang", defaultValue = "sc") String lang,
+                                    @RequestParam(name = "page", defaultValue = "0") Integer page,
+                                    @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+
+
+        return this.getOkResponseModel();
+    }
 }
