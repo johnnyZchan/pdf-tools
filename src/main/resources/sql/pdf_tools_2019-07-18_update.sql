2237210e2302d433a082524d2b39cb800cca356c
JohnnyChan <1363025975@qq.com>
2019-07-18 10:48:10 +0800

    坐标修正

---
+++ src/main/resources/sql/pdf_tools_2019-07-18_update.sql
@@ -0,0 +1,13 @@
+UPDATE pdf_data_coordinate SET margin_left=91.12, margin_top=215.85, width=67.13, height=8.46 WHERE id='01';
+UPDATE pdf_data_coordinate SET margin_left=502.54, margin_top=216.29, width=42.10, height=7.61 WHERE id='02';
+UPDATE pdf_data_coordinate SET margin_left=502.54, margin_top=225.57, width=42.10, height=7.61 WHERE id='03';
+UPDATE pdf_data_coordinate SET margin_left=444.83, margin_top=301.23, width=55.15, height=7.61 WHERE id='04';
+UPDATE pdf_data_coordinate SET margin_left=444.83, margin_top=310.23, width=55.15, height=7.61 WHERE id='05';
+UPDATE pdf_data_coordinate SET margin_left=458.83, margin_top=328.23, width=55.15, height=7.61 WHERE id='06';
+UPDATE pdf_data_coordinate SET margin_left=445.28, margin_top=391.31, width=55.38, height=7.61 WHERE id='07';
+UPDATE pdf_data_coordinate SET margin_left=167.77, margin_top=421.56, width=61.64, height=6.98 WHERE id='08';
+UPDATE pdf_data_coordinate SET margin_left=167.77, margin_top=430.56, width=61.64, height=6.98 WHERE id='09';
+UPDATE pdf_data_coordinate SET margin_left=167.77, margin_top=439.56, width=61.64, height=6.98 WHERE id='10';
+UPDATE pdf_data_coordinate SET margin_left=332.77, margin_top=412.08, width=61.64, height=7.61 WHERE id='11';
+UPDATE pdf_data_coordinate SET margin_left=514.88, margin_top=411.98, width=38.53, height=7.61 WHERE id='12';
+UPDATE pdf_data_coordinate SET margin_left=463.08, margin_top=505.98, width=57.33, height=7.61 WHERE id='13';
