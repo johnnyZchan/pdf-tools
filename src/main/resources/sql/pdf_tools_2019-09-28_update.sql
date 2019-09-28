86ad745b22b8f6d1db23b61377a8833bcafc301a
1363025975@qq.com <1363025975@qq.com>
2019-09-28 20:52:48 +0800

    bug fixed

--- src/main/resources/sql/pdf_tools_2019-09-28_update.sql
+++ src/main/resources/sql/pdf_tools_2019-09-28_update.sql
@@ -7,3 +7,5 @@ alter table pdf_list add column declare_freight_amount_unit varchar(50) null com
 
 INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
  VALUES ('177', 'AIR/IMP', 1, 'list', NULL, 'declareFreightAmountUnit', NULL, NULL, NULL, NULL, 345.86, 310.98, 16.53, 7.25, 'left', 'String', NULL, NULL, NULL, 2, 'aspose');
+
+ update pdf_data_coordinate set margin_top=410.98 where id="12";
