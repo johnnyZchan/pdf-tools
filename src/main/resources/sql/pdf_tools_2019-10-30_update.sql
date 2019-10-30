7423e52560d8fff0d7c265ecce75d6d0b5634a0f
1363025975@qq.com <1363025975@qq.com>
2019-10-30 21:50:22 +0800

    添加删除的位置

---
+++ src/main/resources/sql/pdf_tools_2019-10-30_update.sql
@@ -0,0 +1,48 @@
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('178', 'AIR/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 29.86, 64.79, 36.02, 9.15, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('179', 'AIR/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 494.91, 64.21, 67.23, 8.22, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('180', 'AIR/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 74.36, 702.78, 59.63, 9.02, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('181', 'AIR/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 534.36, 720.78, 31.96, 9.02, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('182', 'AIR/IMP', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 29.87, 74.23, 42.06, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('183', 'AIR/IMP', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 493.58, 74.23, 74.96, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('184', 'AIR/IMP', 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 29.87, 74.23, 42.06, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('185', 'AIR/IMP', 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 493.58, 74.23, 74.96, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('186', 'AIR/IMP', 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 29.87, 74.23, 42.06, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('187', 'AIR/IMP', 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 493.58, 74.23, 74.96, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+
+
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('188', 'SEA/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 29.86, 64.79, 36.02, 9.15, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('189', 'SEA/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 494.91, 64.21, 67.23, 8.22, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('190', 'SEA/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 74.36, 705.78, 59.63, 9.02, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('191', 'SEA/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 534.36, 723.78, 31.96, 9.02, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('192', 'SEA/IMP', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 29.87, 74.23, 42.06, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('193', 'SEA/IMP', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 493.58, 74.23, 74.96, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('194', 'SEA/IMP', 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 29.87, 74.23, 42.06, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('195', 'SEA/IMP', 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 493.58, 74.23, 74.96, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('196', 'SEA/IMP', 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 29.87, 74.23, 42.06, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('197', 'SEA/IMP', 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 493.58, 74.23, 74.96, 9.85, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
