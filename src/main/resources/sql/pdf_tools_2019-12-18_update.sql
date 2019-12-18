e2cc363dda8e3e32fc96573bf29545bda863a269
JohnnyChan <1363025975@qq.com>
2019-12-18 11:10:16 +0800

    增加删除项

---
+++ src/main/resources/sql/pdf_tools_2019-12-18_update.sql
@@ -0,0 +1,6 @@
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('198', 'AIR/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 90, 254, 90, 5, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
+
+
+INSERT INTO `pdf_data_coordinate` (`id`, `template_name`, `page_no`, `field_type`, `field_category`, `field_name`, `llx`, `lly`, `urx`, `ury`, `margin_left`, `margin_top`, `width`, `height`, `align`, `data_type`, `decimal_digits`, `prefix`, `suffix`, `action_type`, `read_type`)
+ VALUES ('199', 'SEA/IMP', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 82, 279, 86, 5, NULL, NULL, NULL, NULL, NULL, 1, 'aspose');
